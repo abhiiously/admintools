5a12f20db5ad294b0655fe333d14be1acf301017
Abhishek Bhatia <abhikbhatia@gmail.com>
2024-07-03 18:23:53 -0700

    Initial program

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Windows.Forms;
+
+namespace AdminTools
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeForm();
+        }
+
+        private void InitializeForm()
+        {
+            Button clickMeButton = new Button
+            {
+                Text = "Click Me",
+                Location = new System.Drawing.Point(100, 100),
+                Size = new System.Drawing.Size(100, 50)
+            };
+
+            clickMeButton.Click += ClickMeButton_Click;
+            Controls.Add(clickMeButton);
+        }
+
+        private void ClickMeButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello from WinForms!");
+        }
+    }
+}
